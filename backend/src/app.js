12ca3bcbec425e5b0302fd5b6ddabfc5051eedaa
khoile903 <khoile903@gmail.com>
2025-11-11 03:06:02 +0700

    update: gRPC and connect fix json dump

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,22 @@ import dotenv from 'dotenv';
 import mainRouter from './routes/router.js';
 import { setUpRole } from './utils/setupRole.js';
 
+// Init client gRCP 
+import grcp from '@grpc/grpc-js';
+import loader from '@grpc/proto-loader';
+
+const PROTO_PATH = '/Users/quockhoile/Desktop/DaiHoc/MLwithGraph/Project/GNN-Model/modern-model/service.proto';
+const packageDefinition = loader.loadSync(PROTO_PATH, {
+  keepCase: true,
+  longs: String,
+  enums: String,
+  defaults: true,
+  oneofs: true
+});
+const protoDescriptor = grcp.loadPackageDefinition(packageDefinition).TestService;
+const grpcClient = new protoDescriptor('localhost:50051', grcp.credentials.createInsecure());
+  
+
 const app = express();
 
 dotenv.config();
@@ -30,6 +46,22 @@ const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello from the backend server!');
+});
+
+// Test gRPC endpoint
+app.get('/grpc-test', (req, res) => {
+    const reqData = {
+        a: req.query.a || 1,
+        b: req.query.b || 3
+    }
+    grpcClient.Addfunction(reqData, (error, response) => {
+        if (error) {
+            console.error('gRPC Error:', error);
+            return res.status(500).send({ message: 'gRPC request failed.' });
+        }
+        console.log('gRPC Response:', response.res);
+        res.send({ message: 'gRPC request successful!', data: JSON.parse(response.res) });
+    });
 });
 
 app.listen(PORT, () => {
--- modern-model/main_test.py
+++ modern-model/main_test.py
@@ -1,15 +1,29 @@
-def print_json(data):
-    import json
-    print(json.dumps(data, indent=4))
-    object = {
-        "message" : "Hello, World!"
-    }
-    return json.dumps(object, indent=4)
+import service_pb2_grpc
+import grpc
+import service_pb2
+from concurrent import futures
+import json
 
-if __name__ == "__main__":
-    django_data = {
-        "name": "Django",
-        "type": "Web Framework",
-        "language": "Python"
-    }
-    print_json(django_data)
+def AddfunctionA(a, b):
+    result = a + b
+    res_json = {"result": result, "message": "Addition successful", "status": "OK"}   
+    return json.dumps(res_json)
+
+
+class IML(service_pb2_grpc.TestServiceServicer):
+    def Addfunction(self, request, context):
+        a = request.a
+        b = request.b
+        res_str = AddfunctionA(a, b)
+        return service_pb2.Result(res=res_str)
+    
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    service_pb2_grpc.add_TestServiceServicer_to_server(IML(), server)
+    server.add_insecure_port('[::]:50051')
+    server.start()
+    print("Server started on port 50051")
+    server.wait_for_termination()
+
+if __name__ == '__main__':
+    serve()
